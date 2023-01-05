e029761cb0e500ae1629ac61f91bab22998f0f98
Serge Basangovs <sergejs.basangovs@strike.co.uk>
2023-01-05 14:46:05 +0000

    feat: middleware - set header

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,18 @@
 import { NextResponse, NextMiddleware } from 'next/server';
 
 export const middleware: NextMiddleware = (req, event) => {
-  const res = NextResponse.next();
-  console.log('path: ', req.nextUrl.pathname);
-  console.log('request/ my secret cookie: ', req.cookies.get('x-httpOnly-my-cookie')?.value);
+  const res = NextResponse.next({
+    request: {
+      headers: new Headers(req.headers),
+    },
+  });
 
-  res.cookies.set('x-httpOnly-my-cookie', 'wow-some-secret-value', { httpOnly: true });
+  const mySecret = req.cookies.get('x-httpOnly-my-cookie')?.value;
+  console.log('path: ', req.nextUrl.pathname);
+  console.log('request/ my secret cookie: ', mySecret);
+
+  !!mySecret && res.headers.set('x-my-secret', mySecret);
+  !mySecret && res.cookies.set('x-httpOnly-my-cookie', 'wow-some-secret-value', { httpOnly: true });
   res.cookies.set('x-visible-my-cookie', 'on-client-visible-value');
 
   return res;
