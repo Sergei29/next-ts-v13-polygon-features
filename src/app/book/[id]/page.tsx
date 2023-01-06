a69308b4f4434e841a01c7351cdbceb336351f5d
Serge Basangovs <sergejs.basangovs@strike.co.uk>
2023-01-06 17:24:16 +0000

    feat: jsdoc comment added

--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -10,6 +10,14 @@ type Props = {
   params: { id: string };
 } & Record<string, any>;
 
+/**
+ * @description currently - IF using the SSG - there is a build error:
+ * `Collecting page data ..ApolloError: fetch failed. Failed to collect page data for /book/[id]`
+ * so temporary fall back using client side fetching.
+ * apparently while using `generateStaticParams` fetch by apollo client - it fails
+ * BUT it also fails using old method `getStaticPaths`, so posibly there is an error in
+ * settin up the Apollo client.
+ */
 const BookPage = ({ params }: Props) => {
   const { data, error, loading } = useQuery<{ book: Book }>(GET_BOOK_BY_ID, {
     variables: { id: params.id },
