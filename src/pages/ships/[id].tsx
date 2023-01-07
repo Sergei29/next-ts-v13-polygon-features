08d998a8dc8d0b84efe475854abb733346fee901
Sergejs Basangovs <sergejs.basangovs@gmail.com>
2023-01-07 20:01:08 +0200

    fix: type error fixed

--- src/pages/ships/[id].tsx
+++ src/pages/ships/[id].tsx
@@ -1,57 +1,13 @@
-import type { NextPage, GetServerSideProps } from "next";
+import type { NextPage } from "next";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useQuery } from "@apollo/client";
 
-import { GET_SHIP_BY_ID, apolloClient } from "@/graphql/client";
+import { GET_SHIP_BY_ID } from "@/graphql/client";
 import ShipParticulars from "@/components/ShipParticulars";
 import { ShipDetails } from "@/types";
 
-const getShipDetails = async (
-  id: string
-): Promise<{
-  ship: ShipDetails | null;
-  error: string | null;
-}> => {
-  try {
-    const { data, error } = await apolloClient.query<{ ship: ShipDetails }>({
-      query: GET_SHIP_BY_ID,
-      variables: { id },
-    });
-
-    if (!!error) {
-      throw new Error(error.message);
-    }
-
-    return { ship: data.ship, error: null };
-  } catch (error) {
-    return {
-      ship: null,
-      error: (error as Error).message || "Failed to get ship details",
-    };
-  }
-};
-
-/**
- * @description for some reason, there was a problem recently to generate statically page while using `apolloCLient.query()` within the `GetStaticProps` in build-time, so as alternative using `getServerSideProps` instead.
- */
-// export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-//   const id = params?.id as string;
-
-//   const { ship, error } = await getShipDetails(id);
-
-//   return {
-//     props: {
-//       ship,
-//       error,
-//     },
-//   };
-// };
-
-type PageProps = {
-  // ship: ShipDetails | null;
-  // error: string | null;
-};
+type PageProps = {};
 
 const ShipDetailsPage: NextPage<PageProps> = () => {
   const router = useRouter();
