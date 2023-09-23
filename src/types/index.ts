bce317bddf2512eea40a7222d43c683487e1ec1a
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-09-23 11:28:02 +0300

    feat: upgraded to lates, added common files

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+export interface PageProps<
+  P = Record<string, string>,
+  Q = Record<string, string>
+> {
+  params: P;
+  searchParams: Q;
+}
+
+export interface ParentProps {
+  children: ReactNode;
+}
