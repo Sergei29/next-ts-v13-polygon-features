2be7f6dcc8f7de86d523273733db9b36af5306e1
Serge Basangovs <sergejs.basangovs@gmail.com>
2023-10-08 16:29:42 +0300

    feat: api route handler updated

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -1,5 +1,24 @@
-import { NextRequest, NextResponse } from "next/server"
+import { NextResponse } from "next/server"
 
-export const GET = (req: NextRequest, res: NextResponse) => {
+export const GET = (req: Request) => {
   return NextResponse.json({ message: "Hello world!" })
 }
+
+/**
+ * @description
+ *
+ * for getting request body: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#request-body
+ *
+ * for dynamic route segments: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
+ *
+ * for search params: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#redirects,
+ *  https://nextjs.org/docs/app/building-your-application/routing/route-handlers#opting-out-of-caching
+ *
+ * for cookies and headers: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#dynamic-route-segments
+ *
+ * setting cookies: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#cookies
+ *
+ * for redirects: https://nextjs.org/docs/app/building-your-application/routing/route-handlers#redirects
+ *
+ *
+ */
